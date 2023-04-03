bf897539700ae31f7b3c34d4dfb3c26c7b559a6a
jon.roji <jon.roji@sngular.com>
2023-04-03 20:45:02 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
 import './App.css'
+import Home from './views/Home'
+import Contact from './views/Contact'
 
 function App() {
 
-  return (<h1>Proyecto Gamma</h1>)
+  return (
+    <>
+    <header>
+      <h1>Proyecto Gamma</h1>
+      <Link to="/contact">Contacto</Link>
+      <Link to="/">Home</Link>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+    </>
+  )
 }
 
 export default App
---
+++ src/views/Contact.jsx
@@ -0,0 +1,5 @@
+export default function Contact() {
+    return <h1>Hola, soy el contacto
+        <p>BLA BLA BLA, COntactame, soy tu desarrollador</p>
+    </h1>
+}
