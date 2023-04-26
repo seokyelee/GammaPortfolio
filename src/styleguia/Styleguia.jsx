d7200e592dfb9814e95a21d27aa39ddb68997f4f
Rosa <=>
2023-04-26 20:53:45 +0200

    more components

--- src/styleguia/Styleguia.jsx
+++ src/styleguia/Styleguia.jsx
@@ -6,11 +6,10 @@ import Btn from '../componentes/Btn/Btn';
 export default function Styleguia() {
 
     return (
+
         <>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace&display=swap" rel="stylesheet"></link>
         
+
             <h1 className="sg__h1">Hola, soy styleguia</h1>
             <Nav />
             <Btn />
