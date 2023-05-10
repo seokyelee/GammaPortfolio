52fb7b8c25adce7c803b57f2e99f44c916272887
Rosa <=>
2023-05-10 20:51:05 +0200

    add funtion

--- src/componentes/Btn/Btn.jsx
+++ src/componentes/Btn/Btn.jsx
@@ -6,8 +6,10 @@ export default function Btn() {
     return (
         <>
         <div className="btn"></div>
-            <button className="btn__primary">Click</button>
-            <button className="btn__primary__disable">Click</button>
+            <button className="btn__pri">Click</button>
+            <button className="btn__pri__disable">Click</button>
+            <button className="btn__error">Click</button>
+            <button className="btn__sec">Click</button>
           
         
         </>
