ef17a1e2d2fa81d50076d61da4e519644996704d
Rosa <=>
2023-05-10 21:00:18 +0200

    fix function color

--- src/componentes/Btn/Btn.jsx
+++ src/componentes/Btn/Btn.jsx
@@ -10,8 +10,6 @@ export default function Btn() {
             <button className="btn__pri__disable">Click</button>
             <button className="btn__error">Click</button>
             <button className="btn__sec">Click</button>
-          
-        
         </>
     );
 }
