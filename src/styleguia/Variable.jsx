a52656431572dca234759c7ba61582d12ff38334
seokye <seokyelee@gmail.com>
2023-05-11 13:58:54 +0200

    add @import Btn.scss

--- src/styleguia/Variable.jsx
+++ src/styleguia/Variable.jsx
@@ -1,15 +1,49 @@
 export default function Variable() {
 	return (
 		<>
-			<h2>Paleta de colores</h2>
-			<ul>
-				<li className=" disable">color disable:#97A1AF</li>
-				<li className=" cancel"> color cancel:#F20D0D</li>
-				<li className=" bg">color bg:#454F54</li>
-				<li className=" border">color border:#EDE7DE</li>
-				<li className=" p">color p:#EDE7DE</li>
-				<li className=" btn">color btn:#1FADAD</li>
-			</ul>
+			<div className="color__box">
+				<h2>Paleta de colores</h2>
+				<ul>
+					<li className=" disable">color disable:#97A1AF</li>
+					<li className=" cancel"> color cancel:#F20D0D</li>
+					<li className=" bg">color bg:#454F54</li>
+					<li className=" border">color border:#EDE7DE</li>
+					<li className=" p">color p:#EDE7DE</li>
+					<li className=" btn">color btn:#1FADAD</li>
+				</ul>
+			</div>
+
+			<div className="font__box">
+				<h2>
+					h2 - Lorem ipsum dolor sit amet consectetur adipiscing elit donec
+					venenatis morbi sociosqu mattis
+				</h2>
+				<h3>
+					h3 - Lorem ipsum dolor sit amet consectetur adipiscing elit donec
+					venenatis morbi sociosqu mattis
+				</h3>
+				<h4>
+					h4 - Lorem ipsum dolor sit amet consectetur adipiscing elit donec
+					venenatis morbi sociosqu mattis
+				</h4>
+				<h5>
+					h5 - Lorem ipsum dolor sit amet consectetur adipiscing elit donec
+					venenatis morbi sociosqu mattis
+				</h5>
+				<h6>
+					h6 - Lorem ipsum dolor sit amet consectetur adipiscing elit donec
+					venenatis morbi sociosqu mattis
+				</h6>
+				<p>
+					Paragraph - Lorem ipsum dolor sit amet consectetur adipiscing elit
+					donec, venenatis morbi sociosqu mattis porta nullam nulla molestie
+					luctus, convallis mi phasellus augue vestibulum cursus sociis.
+					Dictumst volutpat lectus vivamus congue lobortis vel mattis enim
+					consequat, non pharetra aenean mollis pellentesque tristique faucibus
+					sem rutrum mus, eleifend egestas turpis conubia fames vulputate
+					convallis bibendum.
+				</p>
+			</div>
 		</>
 	);
 }
