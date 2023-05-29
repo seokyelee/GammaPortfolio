f61c182358182a9710e812cf2b432c6e1d427aff
syl <seokyelee@gmail.com>
2023-05-29 15:36:15 +0200

    Organization for new proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './scss/global.scss'
 import './App.css'
-import Home from './views/home/Home'
-import Contact from './views/contact/Contact'
-import Styleguia from './styleguia/Styleguia'
-
 
 function App() {
 
   return (
     <>
-    <header className="section">
-      <h1 className="title">Proyect Gamma</h1>
-      <Link className="page_home" to="/">Home</Link>
-      <Link className="page_style" to="/styleguia">Styleguia</Link>
-      <Link className="page_contact" to="/contact">Contacto</Link>
-    </header>
-    <main>
-      <Routes >
-        <Route path="/" element={<Home />} />
-        <Route path="/styleguia" element={<Styleguia />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </main>
+      <header className="section">
+
+        <Link className="icono_mail" to="https://www.google.com/">
+          <IconMail/>
+        </Link>
+        <Link className="icono_moon" to="/">
+        <IconMoon/>
+        </Link>
+        <Link className="icono_codepen" to="https://codepen.io/seokyelee">
+        <IconCodepen/>
+        </Link>
+        <Link className="icono_git" to="https://github.com/seokyelee">
+        <IconGit/>
+        </Link>
+        <Link className="icono_lnkedin" to="https://www.linkedin.com/in/seok-ye-lee-a8196941/">
+        <IconLinkedin/>
+        </Link>
+        <Link className="page_home" to="/">Home</Link>
+        <Link className="page_work" to="/work">Work</Link>
+        <Link className="page_about" to="/about">My name is Seokye</Link>
+      </header>
+      <main>
+       
+       
+      </main>
+
+
     </>
   );
 }
