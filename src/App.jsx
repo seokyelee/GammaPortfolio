b22bd13ea3234bcb0c6d8eeef8e57e3d09a1ece0
syl <seokyelee@gmail.com>
2023-05-30 15:23:51 +0200

    add css nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import './scss/global.scss';
 import Nav from "./componentes/Nav/Nav";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home/Home";
--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Nav.scss";
 import "../../scss/global.scss";
 import IconMoon from "../../assets/Icons/IconMoon";
@@ -9,57 +9,60 @@ import IconGit from "../../assets/Icons/IconGit";
 import IconLinkedin from "../../assets/Icons/IconLinkedin";
 
 export default function Nav() {
-  return (
-    <>
-      <section>
-        <ul className="nav-box">
-          <li className="nav__item">
-            <Link className="icono_mail" to="https://www.google.com/">
-              <IconMail />
-            </Link>
-          </li>
+	return (
+		<>
+			<section className="nav__container">
+				<ul className="nav__box__left">
+					<li className="nav__item">
+						<Link className="icono_mail" to="https://www.google.com/">
+							<IconMail />
+						</Link>
+					</li>
 
-          <li className="nav__item">
-            <Link className="icono_moon" to="https://www.google.com/">
-              <IconMoon />
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="icono_codepen" to="https://codepen.io/seokyelee">
-              <IconCodepen />
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="icono_git" to="https://github.com/seokyelee">
-              <IconGit />
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link
-              className="icono_lnkedin"
-              to="https://www.linkedin.com/in/seok-ye-lee-a8196941/"
-            >
-              <IconLinkedin />
-            </Link>
-          </li>
-
-          <li className="nav__item">
-            <Link className="page_home" to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="page_work" to="/work">
-              Work
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="page_about" to="/about">
-              My name is Seokye
-            </Link>
-          </li>
-        </ul>
-      </section>
-    </>
-  );
+					<li className="nav__item">
+						<Link className="icono_moon" to="https://www.google.com/">
+							<IconMoon />
+						</Link>
+					</li>
+				</ul>
+				<ul className="nav__box__center">
+					<li className="nav__item">
+						<Link className="icono_codepen" to="https://codepen.io/seokyelee">
+							<IconCodepen />
+						</Link>
+					</li>
+					<li className="nav__item">
+						<Link className="icono_git" to="https://github.com/seokyelee">
+							<IconGit />
+						</Link>
+					</li>
+					<li className="nav__item">
+						<Link
+							className="icono_lnkedin"
+							to="https://www.linkedin.com/in/seok-ye-lee-a8196941/"
+						>
+							<IconLinkedin />
+						</Link>
+					</li>
+				</ul>
+				<ul className="nav__box__right">
+					<li className="nav__item">
+						<Link className="page_home" to="/">
+							Home
+						</Link>
+					</li>
+					<li className="nav__item">
+						<Link className="page_work" to="/work">
+							Work
+						</Link>
+					</li>
+					<li className="nav__item">
+						<Link className="page_about" to="/about">
+							My name is Seokye
+						</Link>
+					</li>
+				</ul>
+			</section>
+		</>
+	);
 }
