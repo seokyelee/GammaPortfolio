32451fda81228063a957dad1a373767cec4a0b77
syl <seokyelee@gmail.com>
2023-05-30 16:50:23 +0200

    contents nav scss

--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -11,32 +11,32 @@ import IconLinkedin from "../../assets/Icons/IconLinkedin";
 export default function Nav() {
 	return (
 		<>
-			<section className="nav__container">
+			<section className="nav">
 				<ul className="nav__box__left">
-					<li className="nav__item">
+					<li className="nav__box__left__item">
 						<Link className="icono_mail" to="https://www.google.com/">
 							<IconMail />
 						</Link>
 					</li>
 
-					<li className="nav__item">
+					<li className="nav__box__left__item">
 						<Link className="icono_moon" to="https://www.google.com/">
 							<IconMoon />
 						</Link>
 					</li>
 				</ul>
 				<ul className="nav__box__center">
-					<li className="nav__item">
+					<li className="nav__box__center__item">
 						<Link className="icono_codepen" to="https://codepen.io/seokyelee">
 							<IconCodepen />
 						</Link>
 					</li>
-					<li className="nav__item">
+					<li className="nav__box__center__item">
 						<Link className="icono_git" to="https://github.com/seokyelee">
 							<IconGit />
 						</Link>
 					</li>
-					<li className="nav__item">
+					<li className="nav__box__center__item">
 						<Link
 							className="icono_lnkedin"
 							to="https://www.linkedin.com/in/seok-ye-lee-a8196941/"
@@ -46,17 +46,17 @@ export default function Nav() {
 					</li>
 				</ul>
 				<ul className="nav__box__right">
-					<li className="nav__item">
+					<li className="nav__box__right__item">
 						<Link className="page_home" to="/">
 							Home
 						</Link>
 					</li>
-					<li className="nav__item">
+					<li className="nav__box__right__item">
 						<Link className="page_work" to="/work">
 							Work
 						</Link>
 					</li>
-					<li className="nav__item">
+					<li className="nav__box__right__item">
 						<Link className="page_about" to="/about">
 							My name is Seokye
 						</Link>
