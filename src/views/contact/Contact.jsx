c47cabaff95bcf827004970deb0105f081a1d6f2
syl <seokyelee@gmail.com>
2023-06-01 10:25:07 +0200

    remove. contact folder and foto svg

--- src/views/contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-    return (
-        <>
-    <h1>Hola, soy el contacto
-        <p>BLA BLA BLA, COntactame, soy tu desarrollador</p>
-    </h1>
-    </>
-    );
-}
