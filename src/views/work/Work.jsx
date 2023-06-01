c7c183a2bbfb74322ec889795d3276a156e6a4cb
syl <seokyelee@gmail.com>
2023-06-01 21:01:12 +0200

    test map whit json

--- src/views/work/Work.jsx
+++ src/views/work/Work.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Card from '../../componentes/Card/Card';
+import data from '../../sample.json';
 
 export default function Work() {
-  return (
-    <div>
-      <Card/>
-    </div>
-  )
+  const filteredArray = data.entries.filter(obj => obj.releaseYear >= 2010);
+  const slicedArray = filteredArray.slice(0, 20);
+  
+
+  return <>
+      { slicedArray.map(item => {
+          return <Card key={item.id} image={item.images?.['Poster Art'].url} text={item.title}></Card>
+      })}
+  </>
 }
+
