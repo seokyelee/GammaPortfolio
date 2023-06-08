3c8d82ad255325621874e0428b912401fe730481
syl <seokyelee@gmail.com>
2023-06-08 02:15:55 +0200

    tried option language TypeIt

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import TypeIt from "typeit-react";
 import './Home.scss';
 import '../../componentes/Footer/Footer';
@@ -6,33 +6,37 @@ import Footer from "../../componentes/Footer/Footer";
 
 
 export default function Home() {
+
+
+
     return (
         <>
-       <main>
-       <div className="typeit_container">
-                <TypeIt className="typeit"
-                    getBeforeInit={(instance) => {
-                        instance.type("I want to be")
-                            .pause(2000).delete(12).pause(1800)
-                            .type("a Good Developer").pause(2000)
-                            .delete(9).pause(1200)
-                            .type("Designer").pause(2000)
-                            .delete(15).pause(1200)
-                            .type("but above all").pause(2200)
-                            .delete(13).pause(2000)
-                            .type("a Good") .pause(1000).type(" Person...")
-                        // Remember to return it!
-                        return instance;
-                    }}
-                />
-            </div>
-        </main>
+            <main>
+                <div className="typeit_container">
+                    <TypeIt className="typeit"
+                        getBeforeInit={(instance) => {
+                            instance.type("I want to be")
+                                .pause(2000).delete(12).pause(1800)
+                                .type("a Good Developer").pause(2000)
+                                .delete(9).pause(1200)
+                                .type("Designer").pause(2000)
+                                .delete(15).pause(1200)
+                                .type("but above all").pause(2200)
+                                .delete(13).pause(2000)
+                                .type("a Good").pause(1000).type(" Person...")
+                            // Remember to return it!
+                            return instance;
 
-        <footer>
+                        }}
+                    />
+                </div>
+            </main>
 
-        <Footer/>
+            <footer>
 
-        </footer>
+                <Footer />
+
+            </footer>
 
         </>
     );
