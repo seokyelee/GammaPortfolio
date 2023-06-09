bf03ac0a4a7405545814b035e591bdd27d925515
syl <seokyelee@gmail.com>
2023-06-09 23:31:58 +0200

    modified nav mediaquery

--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -29,53 +29,54 @@ export default function Nav() {
 	return (
 		<>
 			<section className={`nav__container ${themeContext.theme === "dark" ? "dark" : ""}`}>
-				<div className="nav__item__mail_1">
-					<Link className="icono_mail__page_thanks" to="/thanks">
-						<IconMail />
-					</Link>
-				</div>
+				
 
-				<div className="nav__item__codepen_3">
+				<div className="nav__item__codepen">
 					<Link className="icono_codepen" to="https://codepen.io/seokyelee">
 						<IconCodepen />
 					</Link>
 				</div>
-				<div className="nav__item__git_4">
+				<div className="nav__item__git">
 					<Link className="icono_git" to="https://github.com/seokyelee">
 						<IconGit className="zoom" />
 					</Link>
 				</div>
-				<div className="nav__item__linkedin_5">
+				<div className="nav__item__linkedin">
 					<Link className="icono_linkedin" to="https://www.linkedin.com/in/seok-ye-lee-a8196941/">
 						<IconLinkedin />
 					</Link>
 				</div>
 
-				<div className="nav__item__home_6">
+				<div className="nav__item__home">
 					<Link className="page_home" style={textStyle} to="/">
 						Home
 					</Link>
 				</div>
-				<div className="nav__item__work_7">
+				<div className="nav__item__work">
 					<Link className="page_work" style={textStyle} to="/work">
 						Work
 					</Link>
 				</div>
-				<div className="nav__item__about_8">
+				<div className="nav__item__about">
 					<Link className="page_about" style={textStyle} to="/about">
 						I'm Seokye.L
 					</Link>
 				</div>
 
 				<div className="toggle_btn_language">
-					<button className={`toggle_btn ${languageContext.language === "en" ? "active" : ""}`} onClick={handleLanguageToggle}>
+					<button style={textStyle} className={`toggle_btn ${languageContext.language === "en" ? "active" : ""}`} onClick={handleLanguageToggle}>
 						{languageContext.language === "en" ? "En" : "Es"}
 					</button>
 				</div>
 				<div className="toggle_btn_theme">
-					<button className={`toggle_btn ${themeContext.theme === "dark" ? "active" : ""}`} onClick={handleThemeToggle}>
+					<button style={textStyle} className={`toggle_btn ${themeContext.theme === "dark" ? "active" : ""}`} onClick={handleThemeToggle}>
 						{themeContext.theme === "dark" ? "Dark" : "Light"}
 					</button>
+				</div>
+				<div className="nav__item__mail">
+					<Link className="icono_mail__page_thanks" to="/thanks">
+						<IconMail />
+					</Link>
 				</div>
 			</section>
 		</>
