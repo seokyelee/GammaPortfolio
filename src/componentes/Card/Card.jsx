412b95bab03c3e4168a64d2b301743f687c021c7
syl <seokyelee@gmail.com>
2023-06-11 00:57:59 +0200

    card comp changeed hover en modo dark

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,45 +1,58 @@
 import React, { useContext, useState } from "react";
 import "./Card.scss";
+
 import { ThemeContext } from "../../App";
 
+
 function Card(props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const themeContext = useContext(ThemeContext);
   let [classText, setClassText] = useState("");
 
-  const themeContext = useContext(ThemeContext);
-
   const textStyle = {
-		color: themeContext.theme === 'dark' ? '#c8e6e4' : '',
-	  };
-    const shadowStyle = {
-      boxShadow: themeContext.theme === 'dark' ? '10px 5px 5px #050049 ' : '',
-      };
+    color: themeContext.theme === "dark" ? "#c8e6e4" : "",
+  };
 
-  function changeClass() {
-    if (classText === "active") {
-      setClassText("");
-    } else {
-      setClassText("active");
-    }
+  const shadowStyle = {
+    boxShadow: themeContext.theme === "dark" ? "10px 5px 5px #050049 " : "",
+  };
+
+  const isDarkMode = themeContext.theme === "dark";
+
+  function handleHover() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovered(false);
   }
 
   return (
-    <section style={shadowStyle} className={"proyect_box " + props.extraClass} onClick={changeClass}>
+    <section
+      className={`proyect_box ${props.extraClass} ${isHovered && isDarkMode ? "dark-mode-hover" : ""
+        }`}
+      style={shadowStyle}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="proyect_box_title">
-        <h1 id="title_usestate" className="ptoyect_titulo">
-          {props.title} 
+        <h1 id="title_usestate" className="ptoyect_titulo" style={textStyle}>
+          {props.title}
           <span className="tooltiptext">{props.tooltip}</span>
         </h1>
-        <p className="preyect_date">{props.date}</p>
-        </div>
-        <div className="proyect_description_box">
-          <p style={textStyle} className="proyect_descrpition">{props.description}</p>
-        </div>
-      
+        <p className="preyect_date" style={textStyle}>{props.date}</p>
+      </div>
+      <div className="proyect_description_box">
+        <p style={textStyle} className="proyect_descrpition">
+          {props.description}
+        </p>
+      </div>
 
-      <div className={"proyect_usestate" + classText}>
-      
-          <p style={textStyle} className="proyect_content">{props.content}</p>
-        
+      <div className={`proyect_usestate ${classText}`}>
+        <p style={textStyle} className="proyect_content">
+          {props.content}
+        </p>
+
         <div className="proyect_links">
           <a className="proyect_linkto" href={props.linkto}>
             Demo
@@ -47,11 +60,9 @@ function Card(props) {
           <a className="proyect_linkto_git" href={props.linktogit}>
             GitHub
           </a>
-          </div>
-          <img src={props.img1} alt="" />
-          <img src={props.img2} alt="" />
-
-       
+        </div>
+        <img src={props.img1} alt="" />
+        <img src={props.img2} alt="" />
       </div>
     </section>
   );
