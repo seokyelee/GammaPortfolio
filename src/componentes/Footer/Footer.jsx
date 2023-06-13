b66c95627574b1e4880acb966dc0b53f1ed066ed
syl <seokyelee@gmail.com>
2023-06-13 18:50:37 +0200

    add text in footer

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -5,30 +5,39 @@ import { Link } from "react-router-dom";
 import { ThemeContext } from "../../App";
 
 export default function Footer() {
-
   const themeContext = useContext(ThemeContext);
 
   return (
     <>
-      <footer >
+      <footer>
         <div className="footer__box">
-        <Link className="page_about" to="/about">
-          <p className={`footer__box_description ${themeContext.theme === "dark" ? "dark" : ""}`}>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit ut, vel at id
-            purus commodo cubilia varius, ullamcorper placerat lacus habitant
-            justo mauris nibh. Non nascetur sagittis volutpat pretium phasellus
-            rutrum cras, sem accumsan semper mi tempus quis, viverra natoque
-            parturient magnis hendrerit ultricies. Lacinia egestas elementum eros
-            imperdiet a cubilia class porttitor dis, rhoncus molestie risus
-            facilisis tristique ornare phasellus sed faucibus habitant, nibh
-            torquent magna senectus praesent vehicula mauris feugiat.
-          </p>
+          <Link className="page_about" to="/about">
+            <p
+              className={`footer__box_description ${
+                themeContext.theme === "dark" ? "dark" : ""
+              }`}
+            >
+              Luis bñuel, George Méliès Ingmar Bergman, Roman Polanski, David Lynch, Fritz Lang ,F.W.
+              Murnau,Stanley Kubrick, Hayao Miyazaki, Akira Kurosawa, Andrei Tarkovsky, François Truffaut Ridley Scott, Pedro
+              Almodóvar D.W. Griffith Paul, Thomas Anderson, Alfonso Cuarón, Wong
+              Kar-Wai,Alejandro Amenábar,Ang Lee, Tim Burton,
+              Krzysztof Kieślowski, Jane Campion, Dalí, Picasso,
+              Van Gogh, Rembrant, Diego
+              Velazquez, Jan van Eyck, Joan Miró, Goya, Klimt, Marc Chagall, Frida Kahalo, Jackson
+              Pollock, Basquiat, Marcel Duchamp, Shin Saimdang, Na Hye-seok : Gatos, café, montaña, brotes, verde,
+              limonero, Ipomoea, mañana, viento, gloomy sunday,
+              sonrisa, rayos de luz, aire, ola, hola, rocío, viñedo, nubes, puesta de sol, aguacero, agosto, septiembre, una
+              rama de árbol, aurora, bosque, tortuga,
+              ballena, canción de mar: Cortesía, saludos,
+              felicitaciones, comprensión, consideración, gratitud, positivismo,
+              empatía, cooperación, responsabilidad, simpatía, generosidad,
+              compartir, respeto, cariño, confianza.
+            </p>
           </Link>
 
           <Link className="page_work" to="/Work">
-          <BacoderSmall className="bacoder_s"/>
+            <BacoderSmall className="bacoder_s" />
           </Link>
-
         </div>
       </footer>
     </>
